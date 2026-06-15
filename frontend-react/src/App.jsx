8bb490c8c7f726663b11e09803697473fa71d918
bunny0111 <shubhamraj0111@gmail.com>
2026-06-15 17:19:44 +0530

    Add auth provider, login, and update routing

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -6,11 +6,13 @@ import Footer from './components/Footer'
 import Register from './components/Register'
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
 import Login from './components/Login'
+import AuthProvider from './AuthProvider'
 
 function App() {
 
   return (
     <>
+    <AuthProvider>
       <BrowserRouter>
       <Header />
         <Routes>
@@ -21,6 +23,7 @@ function App() {
         </Routes>
       <Footer />
       </BrowserRouter>
+    </AuthProvider>
       
       
     </>
