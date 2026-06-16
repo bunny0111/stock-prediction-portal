565d82f83c9e1019920459b2b44a1ba4f6b38fbe
Shubham Raj <shubhamraj0111@gmail.com>
2026-06-16 11:26:55 +0530

    Authentication functionalities

--- frontend-react/src/components/dashboard/Dashboard.jsx
+++ frontend-react/src/components/dashboard/Dashboard.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import {useEffect} from "react";
+import axios from "axios"
+import axiosInstance from "../../axiosInstance"
 
 const Dashboard = () => {
+    useEffect(() => {
+        const fetchProtectedData = async () => {
+            try{
+                const response = await axiosInstance.get('/protected-view/');
+                console.log('Success: ', response.data);
+            }catch(error){
+                console.error('Error fetching data:', error)
+            }
+        }
+        fetchProtectedData();
+    }, [])
+
     return (
         <div className="text-light container">Dashboard</div>
     )
